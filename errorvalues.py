63c0843686859618d8f7a5e564e66034ff1a1e16
Stefan Keller <kestefan@student.ethz.ch>
2014-09-18 21:44:39 +0200

    Implemented errvallist, to work more conveniently with error attached data collections

--- errorvalues.py
+++ errorvalues.py
@@ -182,16 +182,117 @@ class errval(object):
         return errval(np.around(self.val(),n),np.around(self.err(),n),self.printout())
 
 
+class errvallist(list):
+    def __init__(self,vals,errs=0,printout='latex'):
+        if isinstance(vals,errvallist):
+            self.__errl = vals
+        elif isinstance(vals,(list,np.ndarray)) or isinstance(errs,(list,np.ndarray)):
+            if isinstance(vals,(list,np.ndarray)) and isinstance(errs,(list,np.ndarray)):
+                if len(vals)==len(errs):
+                    self.__errl = [errval(vals[j],errs[j],printout) for j in xrange(len(vals))]
+            elif isinstance(vals,(list,np.ndarray)) and isinstance(errs,(int,float,long)):
+                # note: this also covers the case vals is a list of errval entries,
+                # in this case the errs are ignored and the result is a conversion of list to errvallist
+                self.__errl = [errval(v,errs,printout) for v in vals]
+            elif isinstance(vals,(int,float,long)) and isinstance(errs,(list,np.ndarray)):
+                self.__errl = [errval(vals,e,printout) for e in errs]
+        else:
+            raise ValueError, 'Cannot assign input data: {0}'.format(type(vals))
+
+    def __getitem__(self,key):
+        return self.__errl[key]
+    def __setitem__(self,key,value):
+        self.__errl[key]=value
+
+    def __str__(self):
+        outp = '['
+        for evl in self.__errl:
+            outp += evl.__str__()+','
+        outp = outp[:-1]+']'
+        return outp
+    
+    def __iter__(self):
+        # to make the errvallist iterable
+        # i.e. to make the 'in' possible in 'for err in errvallist:'
+        for err in self.__errl:
+            yield err
+
+    def __len__(self):
+        return len(self.__errl)
+
+    def __add__(self,other):
+        if isinstance(other,(errvallist,list)) and len(self)==len(other):
+             errvall = [self[j]+other[j] for j in xrange(len(self))]
+        elif isinstance(other,(int,float,long)):
+            errvall = [s+other for s in self]
+        else:
+            raise TypeError, 'unsupported operand type(s) for +: errval with {0}'.format(type(other))
+        return errvallist(errvall)
+    def __radd__(self,other):
+            return self.__add__(other)
+
+    def __sub__(self,other):
+        if isinstance(other,(errvallist,list)) and len(self)==len(other):
+            errvall = [self[j]-other[j] for j in xrange(len(self))]
+        elif isinstance(other,(int,float,long)):
+            errvall = [s-other for s in self]
+        else:
+            raise TypeError, 'unsupported operand type(s) for -: errval with {0}'.format(type(other))
+        return errvallist(errvall)
+    def __rsub__(self,other):
+        return -1*self.__sub__(other)
+
+    def __mul__(self,other):
+        if isinstance(other,(errvallist,list)) and len(self)==len(other):
+            errvall = [self[j]*other[j] for j in xrange(len(self))]
+        elif isinstance(other,(int,float,long)):
+            errvall = [s*other for s in self]
+        else:
+            raise TypeError, 'unsupported operand type(s) for *: errval with {0}'.format(type(other))
+        return errvallist(errvall)
+    def __rmul__(self,other):
+        return self.__mul__(other)
+
+    def __div__(self,other):
+        if isinstance(other,(errvallist,list)) and len(self)==len(other):
+            errvall = [self[j]/other[j] for j in xrange(len(self))]
+        elif isinstance(other,(int,float,long)):
+            errvall = [s/other for s in self]
+        else:
+            raise TypeError, 'unsupported operand type(s) for /: errval with {0}'.format(type(other))
+        return errvallist(errvall)
+    def __rdiv__(self,other):
+        return 1.0/self.__div__(other)
+
+    def append(self,value):
+        self.__errl.append(value)
+
+    '''
+    Depending on the circumstances the code incorporating this class
+    may want to use different names for the following functions:
+    '''
+    def v(self): return values(self)
+    def val(self): return self.v()
+    def vals(self): return self.v()
+    def values(self): return self.v()
+
+    def e(self): return errors(self)
+    def err(self): return self.e()
+    def errs(self): return self.e()
+    def errors(self): return self.e()
+
 
 '''
-convenience functions for dealing with lists containing errvals
+functions for dealing with lists containing errvals
+not necessarily errvallists
 '''
 def values(errvallist):
-    return [ev.val() for ev in errvallist]
+    return np.array([ev.val() for ev in errvallist])
 def errors(errvallist):
-    return [ev.err() for ev in errvallist]
+    return np.array([ev.err() for ev in errvallist])
 def tuples(errvallist):
     return zip(values(errvallist),errors(errvallist))
+
 def wmean(errvallist):
     '''
     weighted mean
@@ -201,7 +302,9 @@ def wmean(errvallist):
     '''
     printmode = errvallist[0].printout()
     vals = values(errvallist)
+    print vals
     errs = errors(errvallist)
+    print errs
     N = len(errvallist)
     sig_x = np.sum([1.0/si**2 for si in errs])
     sum_x = np.sum([vals[i]*1.0/errs[i]**2 for i in range(N)])
@@ -282,11 +385,20 @@ def main():
     print zip(abcv,abce)
     print abct
     print '---'
+    abc_ = errvallist([a,b,c])
+    print abc_
+    print values(abc_)
+    print abc_.v()
+    print abc_.val()
+    print abc_.errs()
+    print 'exp: {0}\ngot: {1}\n---'.format(f0,np.sum(abc_))
 
     q0, q1 = errval(100,10), errval(1,1)
     r0, r1 = errval(3.11,0.02), errval(3.13,0.01)
     print 'exp: {0}\ngot: {1}\n---'.format(errval(2,1),wmean([q0,q1]))
     print 'exp: {0}\ngot: {1}\n---'.format(errval(3.126,0.009),wmean([r0,r1])) # [R. Barlow, Statistics, John Wiley & Sons Ltd. (1989)]
+    print 'exp: {0}\ngot: {1}\n---'.format(errval(3.126,0.009),wmean(errvallist([r0,r1])))
+    print 'exp: {0}\ngot: {1}\n---'.format(errval(50.5,0.5*np.sqrt(101)),np.mean(errvallist([q0,q1])))
 
 
 if __name__ == '__main__': main()
